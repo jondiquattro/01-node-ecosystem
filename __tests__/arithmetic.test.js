63e364a47e17af68a6964c9f83c87798b403c4d3
Jonathan DiQuattro <jondiquattro@hotmail.com>
2018-12-29 09:12:58 -0800

    added more tests

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -1,17 +1,90 @@
 'use strict';
 
 const arithmetic = require('../lib/arithmetic.js');
+const faker = require('faker');
 
+//test with faker
 it('can add 2 numbers', ()=>{
+    
+  let inputA = faker.random.number();
+  let inputB = faker.random.number();
+  let expected = inputA + inputB;
 
-    let expected = 2;
-    let sum = arithmetic.add(1,1);
-    expect(sum).toEqual(expected);
-})
+  let sum = arithmetic.add(inputA, inputB);
+  expect(sum).toEqual(expected);
+});
+
+//test with hard coded inputs
+it('can add 2 hard coded numbers', ()=>{
+    
+  let inputA = 1;
+  let inputB = 2;
+  let expected = inputA + inputB;
+    
+  let sum = arithmetic.add(inputA, inputB);
+  expect(sum).toEqual(expected);
+});
+
+//test with one or more char inputs
+
+xit('can add 2 numbers', ()=>{
+    
+  let inputA = 'a';
+  let inputB = faker.random.number();
+  let expected = inputA + inputB;
+    
+  let sum = arithmetic.add(inputA, inputB);
+  expect(sum).toEqual(expected);
+});
 
 
+//tested with faker
+it('can add 2 numbers', ()=>{
+    
+  let inputA = faker.random.number();
+  let inputB = faker.random.number();
+  let expected = inputA - inputB;
+  
+  let less = arithmetic.subtract(inputA, inputB);
+  expect(less).toEqual(expected);
+});
+
+//tested with harde coded inputs
 it('can subtract one number from another', ()=> {
-    let expected = 1;
-    let less = arithmetic.subtract(2,1);
-    expect(less).toEqual(expected);
-})
+  let expected = 1;
+  let less = arithmetic.subtract(2,1);
+  expect(less).toEqual(expected);
+});
+
+//tested with one or more chars
+it('can add 2 numbers', ()=>{
+    
+  let inputA = 'a';
+  let inputB = faker.random.number();
+  let expected = inputA - inputB;
+      
+  let less = arithmetic.add(inputA, inputB);
+  expect(less).toEqual(expected);
+});
+
+//division
+
+it('can divide 2 numbers', ()=>{
+    
+  let inputA = faker.random.number();    
+  let inputB = faker.random.number();
+  let expected = inputA / inputB;
+        
+  let division = arithmetic.divide(inputA, inputB);
+  expect(division).toEqual(expected);
+});
+
+it('can multiply 2 numbers', ()=>{
+    
+    let inputA = faker.random.number();    
+    let inputB = faker.random.number();
+    let expected = inputA * inputB;
+          
+    let times = arithmetic.multiply(inputA, inputB);
+    expect(times).toEqual(expected);
+  });
--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -10,3 +10,11 @@ it('can take in a name and return a greeting', ()=>{
   let sum = greet.speak(name);
   expect(sum).toEqual(expected);
 });
+
+it('can take in a name world and return a greeting', ()=>{
+
+    let expected = 'hello world';
+    let sum = greet.speak('world');
+
+    expect(sum).toEqual(expected);
+  });
